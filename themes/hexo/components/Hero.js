54aebdfc8bd119378a02edee9c85384f2886b433
henry <island_1123@163.com>
2026-03-19 13:51:22 +0800

    fix

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -22,32 +22,7 @@ const Hero = props => {
     window.scrollTo({ top: wrapperTop, behavior: 'smooth' })
   }
 
-  const GREETING_WORDS = siteConfig('GREETING_WORDS').split(',')
-  useEffect(() => {
-    updateHeaderHeight()
 
-    if (!typed && window && document.getElementById('typed')) {
-      loadExternalResource('/js/typed.min.js', 'js').then(() => {
-        if (window.Typed) {
-          changeType(
-            new window.Typed('#typed', {
-              strings: GREETING_WORDS,
-              typeSpeed: 200,
-              backSpeed: 100,
-              backDelay: 400,
-              showCursor: true,
-              smartBackspace: true
-            })
-          )
-        }
-      })
-    }
-
-    window.addEventListener('resize', updateHeaderHeight)
-    return () => {
-      window.removeEventListener('resize', updateHeaderHeight)
-    }
-  })
 
   function updateHeaderHeight() {
     requestAnimationFrame(() => {
@@ -62,14 +37,6 @@ const Hero = props => {
       style={{ zIndex: 1 }}
       className='w-full h-screen relative bg-black dark:bg-gray-900'>
       <div className='text-white dark:text-gray-100 absolute bottom-0 flex flex-col h-full items-center justify-center w-full '>
-        {/* 站点标题 */}
-        <div className='font-black text-4xl md:text-5xl shadow-text'>
-          {siteInfo?.title || siteConfig('TITLE')}
-        </div>
-        {/* 站点欢迎语 */}
-        <div className='mt-2 h-12 items-center text-center font-medium shadow-text text-lg'>
-          <span id='typed' />
-        </div>
 
         {/* 首页导航大按钮 */}
         {siteConfig('HEXO_HOME_NAV_BUTTONS', null, CONFIG) && (
